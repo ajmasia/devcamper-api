96435e537765836abfc241ea508e0f17502dec02
Antonio José Masiá <antonio.masia@frogtek.org>
2020-12-30 18:38:53 +0100

    Add initial scripts and a basic server

--- server/app.js
+++ server/app.js
@@ -1,1 +1,12 @@
-console.log('App running ...')
+const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+})
