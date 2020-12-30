11ff36d11b6bd08ec01c6c0c62be1c72abadd967
Antonio José Masiá <antonio.masia@frogtek.org>
2020-12-30 19:00:43 +0100

    Add bootcamps router

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
+// Routes
+const bootcamps = require('./routes/bootcamps')
 dotenv.config()
 
 const app = express()
+
+// Mount routes
+app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 3000
 
