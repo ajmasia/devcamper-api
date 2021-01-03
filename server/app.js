e32908f29c3b57169aa3f140a5652adbee87f4fc
Antonio José Masiá <antonio.masia@frogtek.org>
2021-01-03 16:30:09 +0100

    Add database connection service

--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const connectDB = require('./config/db')
 
 // Routes
 const bootcamps = require('./routes/bootcamps')
+
+// Get env vars
 dotenv.config()
 
+// Connect to de database
+connectDB()
+
+// Create an express app
 const app = express()
 
 // Dev logger middleware
@@ -18,6 +25,13 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+})
+
+// Handle unhandled promises rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`)
+  // Close server & exit process
+  server.close(() => process.exit(1))
 })
---
+++ server/config/db.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+const {
+  DATABASE_DATABASE: db,
+  DATABASE_HOST: host,
+  DATABASE_PASSWORD: pw,
+  DATABASE_PORT: port,
+  DATABASE_REPLICA_SET: replicaSetName,
+  DATABASE_SSL: ssl,
+  DATABASE_USER: user,
+} = process.env
+
+function getConnectionString({
+  host,
+  db,
+  pw,
+  port,
+  replicaSetName,
+  ssl,
+  user,
+}) {
+  const credentials = user ? `${user}:${pw}@` : ''
+  const replicaSetOption = replicaSetName ? `replicaSet=${replicaSetName}` : ''
+  const sslOption = ssl ? `ssl=true` : ''
+
+  return `mongodb://${credentials}${host}:${port}/${db}?${[
+    replicaSetOption,
+    sslOption,
+  ].join('&')}`
+}
+
+const connectionString = getConnectionString({
+  host,
+  db,
+  pw,
+  port,
+  replicaSetName,
+  ssl,
+  user,
+})
+
+const connectDB = async () => {
+  const connection = await mongoose.connect(connectionString, {
+    userNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  console.log(`MongoDB connected: ${connection.connection.host}`)
+}
+
+module.exports = connectDB
