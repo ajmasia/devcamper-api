50ed8b5a467e9cdb077943d333f29ab1ba5eb871
Antonio José Masiá <antonio.masia@frogtek.org>
2021-01-05 17:14:48 +0100

    Add endpoint to get bootcamps by distance

--- server/controllers/bootcamps.js
+++ server/controllers/bootcamps.js
@@ -1,6 +1,7 @@
 const Bootcamp = require('../models/Bootcamp')
 const ErrorResponse = require('../tools/errorResponse')
 const asyncHandler = require('../middleware/asyncHandler')
+const geocoder = require('../tools/geocoder')
 
 // @desc        GET all bootcamps
 // @route       GET /api/v1/bootcamps
@@ -69,10 +70,38 @@ const deleteBootcamp = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: {} })
 })
 
+// @desc      Get bootcamps within a radius
+// @route     GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access    Private
+const getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params
+
+  // Get lat/lng from geocoder
+  const loc = await geocoder.geocode(zipcode)
+  const lat = loc[0].latitude
+  const lng = loc[0].longitude
+
+  // Calc radius using radians
+  // Divide dist by radius of Earth
+  // Earth Radius = 3,963 mi / 6,378 km
+  const radius = distance / 3963
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  })
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  })
+})
+
 module.exports = {
   getBootcamps,
   getBootcamp,
   createBootcamp,
   updateBootcamp,
   deleteBootcamp,
+  getBootcampsInRadius,
 }
--- server/routes/bootcamps.js
+++ server/routes/bootcamps.js
@@ -2,10 +2,12 @@ const express = require('express')
 const controller = require('../controllers/bootcamps')
 const router = express.Router()
 
-router.route('/').get(controller.getBootcamps)
-router.route('/:id').get(controller.getBootcamp)
-router.route('/').post(controller.createBootcamp)
-router.route('/:id').put(controller.updateBootcamp)
-router.route('/:id').delete(controller.deleteBootcamp)
+router.route('/').get(controller.getBootcamps).post(controller.createBootcamp)
+router
+  .route('/:id')
+  .get(controller.getBootcamp)
+  .put(controller.updateBootcamp)
+  .delete(controller.deleteBootcamp)
+router.route('/radius/:zipcode/:distance').get(controller.getBootcampsInRadius)
 
 module.exports = router
