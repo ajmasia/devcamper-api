ab35859b9b7ce7830b4229d1b5231c207b683882
Antonio José Masiá <antonio.masia@frogtek.org>
2021-01-22 18:55:05 +0100

    Add Course model and update seeder script to seed courses data

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config()
 
 // Load models
 const Bootcamp = require('./server/models/Bootcamp')
+const Course = require('./server/models/Course')
 
 // Connect to database
 const {
@@ -40,11 +41,15 @@ const connection = mongoose.connect(connectionString, {
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 )
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+)
 
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps)
+    await Course.create(courses)
     console.log('Data Imported...'.green.inverse)
     process.exit()
   } catch (err) {
@@ -56,6 +61,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany()
+    await Course.deleteMany()
     console.log('Data Destroyed...'.red.inverse)
     process.exit()
   } catch (err) {
